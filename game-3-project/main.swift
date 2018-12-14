f4d110eb47c801b91160b4dc6eef3a9b8634f0ec
lauriane75 <lauriane.haydari@gmail.com>
2018-12-14 14:14:55 +0100

    add first commit

---
+++ game-3-project/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  game-3-project
+//
+//  Created by Lauriane Haydari on 14/12/2018.
+//  Copyright © 2018 Lauriane Haydari. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
