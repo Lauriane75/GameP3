aa1ab629230ba5c53d334396448faecc12224d5e
lauriane75 <lauriane.haydari@gmail.com>
2018-12-19 13:55:58 +0100

    first chaging Game class, func startGame() and createTeam()

--- game-3-project/classes/Game.swift
+++ game-3-project/classes/Game.swift
@@ -8,70 +8,31 @@
 
 import Foundation
 
+
 class Game {
     
-    func input() -> Int {
-        let strData = readLine();
-        return Int(strData!)!
+     var tabPlayer = [Player]() // Array of 2 players team1 & team
+
+    func startGame() {
+        _ = createTeam
     }
+    
+    
+    // compose a team
+   func createTeam() -> Player {
+    let teamName = ""
+        repeat {
+        } while teamName == ""
+    let team = Player(namePlayer: teamName)
+    team.createYourTeam()
+        return team
+    }
+}
+    
+    
+
     
 
 // players1 or player2 ?
    
-
-    
-    var currentCharacter1 = Player1(CurrentCharacterChoice)
-    var currentCharacter2 = Player2(CurrentCharacterChoice)
-    
-    
-    func startFight(currentCharacter1 against : currentCharacter2) {
-        Characters.hurt(injuries: self.weapon.injuries)
-    }
-    
-    // conditions de jeu tant qu'il y a des points de défense
-while currentCharacter1.defense > 0 && currentCharacter2.defense > 0 {
-    currentCharacter1.startFight(against: currentCharacter2)
-    
-    
-    if currentCharacter2.defense > 0 {
-    currentCharacter2.startFight(against: currentCharacter1)
-    }
-}
-    
-var currentCharacter1.defense > 0 {
-
-    if currentCharacter1.defense = currentCharacter1
-        winner = currentCharacter1
-    } else {
-        winner = currentCharacter2
-    
-    }
-    
-    // the winner is printing
-    print("winner")
-    
-
-    
-        
-    }
-    
-    
-    
-   /* var players = [Players]()
-    var currentPlayer: Players?
-    var opponent: Players?
-    var ended: Bool {
-        
-        //        for player in players {
-        //            if player.hasLost {
-        //                return true
-        //            }
-        //        }
-        
-        return false
-    
-    func play(character: Characters) {
-    }*/
-
-
 
