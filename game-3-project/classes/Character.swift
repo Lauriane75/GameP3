cfb1c6911926d8329c0654f0aff07658cff5e3da
lauriane75 <lauriane.haydari@gmail.com>
2018-12-28 09:40:13 +0100

    Add func fight & func cure

--- game-3-project/classes/Character.swift
+++ game-3-project/classes/Character.swift
@@ -16,48 +16,93 @@ class Character {
     var attackPoints: Int
     var weapon: Weapon
     let type: String
+    let fullLifeBar: Int
 
     
     // init values
-    init(nameCharacter: String,defensePoints: Int,attackPoints: Int,weapon: Weapon, type: String) {
+    init(nameCharacter: String,defensePoints: Int,attackPoints: Int,weapon: Weapon, type: String, fullLifeBar: Int) {
        self.nameCharacter = nameCharacter
        self.defensePoints = defensePoints
        self.attackPoints = attackPoints
        self.weapon = weapon
        self.type = type
+       self.fullLifeBar = fullLifeBar
     }
+    
+    func fight(targetCharacter: Character) {
+        if defensePoints > 0 {
+            if targetCharacter.defensePoints <= 0 {
+                print("This guy is already over!")
+            } else {
+                targetCharacter.defensePoints -= weapon.injuries  // to take the defense points of the character enemy
+                if targetCharacter.defensePoints <= 0 {
+                    targetCharacter.defensePoints = 0
+                }
+                print("Your \(type) \(nameCharacter) just hit \(targetCharacter.type) \(targetCharacter.nameCharacter)   whith his \(weapon.nameWeapon) taking \(weapon.injuries) defense points to him, .")
+            }
+        } else {
+            print("Sorry, you need to be revived! ")
+        }
+    }
+    
+    
+    
     
 }
     
 
 class Fighter: Character {
     init(name: String) {
-        super.init(nameCharacter: name, defensePoints: 100,attackPoints: 10, weapon:Sword(), type: "Fighter")
+        super.init(nameCharacter: name, defensePoints: 100,attackPoints: 10, weapon:Sword(), type: "Fighter", fullLifeBar: 100)
     }
 }
 
 
 class Magus: Character {
     init(name: String) {
-        super.init(nameCharacter: name, defensePoints: 300,attackPoints: 0, weapon:MagicHealing(), type: "Magus")
+        super.init(nameCharacter: name, defensePoints: 300,attackPoints: 0, weapon:MagicHealing(), type: "Magus", fullLifeBar: 300)
     }
+    
+    
+    
+    func cure(character: Character) {
+        if defensePoints > 0 {
+            if character.defensePoints <= 0 {
+                print("I'm sorry this buddy just died")
+            } else {
+                character.defensePoints += weapon.healing  // give 50 life points to the character designated
+                if character.defensePoints >= character.fullLifeBar { // to block the defense points at 100% for each character
+                    character.defensePoints = character.fullLifeBar
+                    print("Your \(character.type) \(character.nameCharacter) has all his life points.")
+                }
+                print("Your Magus just gave 50 defense points to your \(character.type) \(character.nameCharacter)") // spell's effects display
+            }
+        } else {
+            print("Your Magus is dead, find an Elixir potion to bring it back to life.")
+        }
+    }
+    
 }
+    
+
+    
+
 
 
 
 class Colossus: Character {
     init(name: String) {
-        super.init(nameCharacter: name, defensePoints: 400,attackPoints: 5, weapon:Hammer(), type: "Colossus")
+        super.init(nameCharacter: name, defensePoints: 400,attackPoints: 5, weapon:Hammer(), type: "Colossus", fullLifeBar: 400)
     }
 }
 
 class Dwarf: Character {
     init(name: String) {
-        super.init(nameCharacter: name, defensePoints: 50,attackPoints: 50, weapon:Axe(), type: "Dwarf")
+        super.init(nameCharacter: name, defensePoints: 70,attackPoints: 50, weapon:Axe(), type: "Dwarf", fullLifeBar: 70)
     }
 }
 
-let availableCharacterTypes: [Character.Type] = [Fighter.self, Magus.self, Colossus.self, Dwarf.self]
+//let availableCharacterTypes: [Character.Type] = [Fighter.self, Magus.self, Colossus.self, Dwarf.self]
 
 
 
